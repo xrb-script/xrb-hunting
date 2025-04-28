ec2984c68686c70947e4191411a878862042ed6f
xrb-script <truexrb@gmail.com>
2025-04-28 21:48:06 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,14 +4,12 @@ lua54 'yes'
 
 author 'xResuL Albania'
 description 'Skining Script by xResuL Albania for qbcore and esx'
-version '1.1.0'
+version '1.2.0'
 
 dependencies {
     'ox_lib',
     'ox_target',
     'ox_inventory',
-    'es_extended',  -- opsionale
-    --'qb-core'       -- opsionale
 }
 
 shared_scripts {
