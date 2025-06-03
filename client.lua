abb4fec7bd37d281f1abe12b98e6f8a4931c01cf
xrb-script <179358921+xrb-script@users.noreply.github.com>
2025-06-03 21:22:06 +0200

    Update client.lua

--- client.lua
+++ client.lua
@@ -2,7 +2,6 @@ local ESX = nil
 local QBCore = nil
 local PlayerData = {}
 
-
 CreateThread(function()
     Wait(500)
     if Config.Framework == 'esx' then
@@ -11,14 +10,9 @@ CreateThread(function()
             ESX = esxExport:getSharedObject()
             if ESX then
                 print("[xrb-Hunting] ESX Framework Detected and Initialized.")
-                -- You might want to get player data here if needed later
-                -- AddEventHandler('esx:playerLoaded', function(xPlayer) PlayerData = xPlayer end)
-                -- TriggerServerEvent('esx:getSharedObject', function(obj) ESX = obj end) -- Alternative way
             else
-                print("[xrb-Hunting] ERROR: Could not get ESX Shared Object. Is es_extended started and updated?")
+                print("[xrb-Hunting] ERROR: Could not get ESX Shared Object.")
             end
-        else
-            print("[xrb-Hunting] ERROR: es_extended export not found. Is es_extended started?")
         end
     elseif Config.Framework == 'qbcore' then
         local qbcoreExport = exports['qb-core']
@@ -26,29 +20,21 @@ CreateThread(function()
             QBCore = qbcoreExport:GetCoreObject()
             if QBCore then
                 print("[xrb-Hunting] QBCore Framework Detected and Initialized.")
-                -- Get player data if needed
-                -- AddEventHandler('QBCore:Client:OnPlayerLoaded', function() PlayerData = QBCore.Functions.GetPlayerData() end)
-                -- RegisterNetEvent('QBCore:Client:OnPlayerUnload', function() PlayerData = {} end)
-                -- PlayerData = QBCore.Functions.GetPlayerData() -- Initial grab
             else
-                 print("[xrb-Hunting] ERROR: Could not get QBCore Object. Is qb-core started and updated?")
+                print("[xrb-Hunting] ERROR: Could not get QBCore Object.")
             end
-        else
-             print("[xrb-Hunting] ERROR: qb-core export not found. Is qb-core started?")
         end
     else
-        print("[xrb-Hunting] ERROR: Invalid framework specified in config.lua. Please set Config.Framework to 'esx' or 'qbcore'.")
+        print("[xrb-Hunting] ERROR: Invalid framework specified.")
     end
 end)
 
 local ox_inventory = exports.ox_inventory
 local ox_target = exports.ox_target
 
-
 local function Notify(msg, type)
     local notificationType = type or 'inform'
     local isError = (type == 'error')
-
     if Config.Framework == 'esx' and ESX then
         ESX.ShowNotification(msg) 
     elseif Config.Framework == 'qbcore' and QBCore then
@@ -58,28 +44,24 @@ local function Notify(msg, type)
     end
 end
 
-
-
 local function skinAnimal(entity)
     local playerPed = PlayerPedId()
-
     if not DoesEntityExist(entity) then return Notify('The animal does not exist.', 'error') end
-
     if IsPedInAnyVehicle(playerPed, false) then
         return Notify('You cant get out of the car!', 'error')
     end
 
     local animalModelHash = GetEntityModel(entity)
     local animalData = Config.GetAnimalDataByHash(animalModelHash)
-
     if not animalData then return Notify('This animal cannot be skinned.', 'error') end
-
 
     if ox_inventory:Search('count', 'skining_knife') < 1 then
         return Notify('I need a skinning knife!', 'error')
     end
-    lib.callback('hunting:checkInventorySpace', false, function(canCarry)
+
+    lib.callback('xrb-hunting:checkInventorySpace', false, function(canCarry)
         if not canCarry then return Notify('You have no space in your inventory.', 'error') end
+
         local success = lib.progressBar({
             duration = 5000,
             label = 'Skining...',
@@ -90,15 +72,13 @@ local function skinAnimal(entity)
         })
 
         if not success then
-            -- ClearPedTasks(playerPed) -- No need to clear if progressbar handles cancellation anims
             return Notify('Skinning was stopped', 'error')
         end
 
-        TriggerServerEvent('hunting:skinAnimal', animalData)
+        TriggerServerEvent('xrb-hunting:skinAnimal', animalData)
         DeleteEntity(entity)
     end, animalData)
 end
-
 
 CreateThread(function()
     Wait(1000)
@@ -107,17 +87,12 @@ CreateThread(function()
             label = 'Skin ' .. (data.name or 'Animal'),
             icon = 'fa-solid fa-bone',
             distance = 2.0,
-            canInteract = function(entity, distance, coords, name, bone)
-                return IsEntityDead(entity)
-            end,
-            onSelect = function(data)
-                skinAnimal(data.entity)
-            end
+            canInteract = function(entity) return IsEntityDead(entity) end,
+            onSelect = function(data) skinAnimal(data.entity) end
         })
     end
     print("[xrb-Hunting] Animal targets added.")
 end)
-
 
 CreateThread(function()
     local pedInfo = Config.SellPed
@@ -130,26 +105,25 @@ CreateThread(function()
     SetBlockingOfNonTemporaryEvents(sellPed, true)
     SetEntityAsMissionEntity(sellPed, true, true)
 
-    -- Blip
     local blip = AddBlipForCoord(pedInfo.coords.xyz)
     SetBlipSprite(blip, 141)
-    SetBlipColour(blip, 2)   
+    SetBlipColour(blip, 2)
     SetBlipScale(blip, 0.8)
     SetBlipAsShortRange(blip, true)
     BeginTextCommandSetBlipName('STRING')
     AddTextComponentString('Meat Seller')
     EndTextCommandSetBlipName(blip)
 
-
     ox_target:addLocalEntity(sellPed, {
         label = 'Sell Products',
-        icon = 'fa-dollar-sign',
+        icon = 'fa-solid fa-dollar-sign',
         distance = 2.5,
         onSelect = function()
-            TriggerServerEvent('hunting:sellProducts')
+            TriggerServerEvent('xrb-hunting:sellProducts')
         end
     })
-     print("[xrb-Hunting] Sell Ped created.")
+
+    print("[xrb-Hunting] Sell Ped created.")
 end)
 
 AddEventHandler('onResourceStop', function(resourceName)
